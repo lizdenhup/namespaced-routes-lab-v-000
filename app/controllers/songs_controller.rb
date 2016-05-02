72a03ffade12e590d4ad0546a2b7ebc8cf607dad
lizdenhup <lizdenhup@gmail.com>
2016-05-02 11:24:37 -0500

    all tests passing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,12 @@ class SongsController < ApplicationController
   end
 
   def new
+    preference = Preference.first 
+    if preference && !preference.allow_create_songs
+      redirect_to songs_path 
+    else 
     @song = Song.new
+    end 
   end
 
   def create
